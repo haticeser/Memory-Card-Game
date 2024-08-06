526735029a4a29ce3e97ff8491d4662e05bf0bd0
Hatice S. ESER <161969056+haticeser@users.noreply.github.com>
2024-08-06 20:22:25 +0300

    Add files via upload

---
+++ Memory Card Game/MemoryGame.js
@@ -0,0 +1,38 @@
+let gameBox = document.querySelector(".container .game");
+
+
+const emojis = ["😇","😇","😍","😍","🤑" ,"🤑","🥶","🥶","😈","😈","💙","💙","🙃","🙃","😴","😴"];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+
+for(let i = 0 ; i < emojis.length ; i++){
+    let box = document.createElement("div");
+    box.classList.add("item");
+
+    box.onclick = (e)=>{
+        e.target.classList.add("boxOpen");
+        let boxes = document.querySelectorAll(".boxOpen");
+        setTimeout(()=>{
+            if(boxes.length > 1)
+            {
+                if(boxes[0].innerHTML == boxes[1].innerHTML){
+                    boxes[0].classList.add("boxMatch");
+                    boxes[1].classList.add("boxMatch");
+                    boxes[1].classList.remove("boxOpen");
+                    boxes[0].classList.remove("boxOpen");
+
+                    if(boxes.lenght == emojis.length){
+                        window.location.reload();
+                    }
+                }else{
+                    boxes[1].classList.remove("boxOpen");
+                    boxes[0].classList.remove("boxOpen");
+                }
+            }
+        },500);
+    }
+
+
+    box.innerHTML = shuffleEmojis[i];
+    gameBox.appendChild(box);
+}
